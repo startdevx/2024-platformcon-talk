22848404a9f7c2dd53e451dddc86abbf38a4157e
Hello <github@devwithkev.com>
2024-04-05 22:48:35 +0200

    Update subscommand update with git commands

--- src/subcommands/update.ps1
+++ src/subcommands/update.ps1
@@ -1,2 +1,17 @@
 Write-Host "Updating CLI"
+
+$currentTagRevision = git -C "$PSScriptRoot\..\.." rev-list --tags --max-count=1
+$currentTagVersion = git -C "$PSScriptRoot\..\.." describe --tags $currentTagRevision
+
 git -C "$PSScriptRoot\..\.." fetch --all --tags
+
+$newTagRevision = git -C "$PSScriptRoot\..\.." rev-list --tags --max-count=1
+$newTagVersion = git -C "$PSScriptRoot\..\.." describe --tags $newTagRevision
+
+if ($currentTagVersion -eq $newTagVersion) {
+    Write-Host "There is not update"
+}
+else {
+    git -C "$PSScriptRoot\..\.." pull
+    Write-Host "Update is successfull"
+}
