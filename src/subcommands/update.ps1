f158aae08fb0aeeaf53f3f1964aa8acd3fb7ee53
Hello <github@devwithkev.com>
2024-04-05 23:13:44 +0200

    Update git logic with git rev-parse

--- src/subcommands/update.ps1
+++ src/subcommands/update.ps1
@@ -1,11 +1,11 @@
 Write-Host "Updating CLI"
 
-$currentTagRevision = git -C "$PSScriptRoot\..\.." rev-list --tags --max-count=1
-$currentTagVersion = git -C "$PSScriptRoot\..\.." describe --tags $currentTagRevision
-
 git -C "$PSScriptRoot\..\.." fetch --all --tags --quiet
 
+$currentTagRevision = git -C "$PSScriptRoot\..\.." rev-parse --verify HEAD
 $newTagRevision = git -C "$PSScriptRoot\..\.." rev-list --tags --max-count=1
+
+$currentTagVersion = git -C "$PSScriptRoot\..\.." describe --tags $currentTagRevision
 $newTagVersion = git -C "$PSScriptRoot\..\.." describe --tags $newTagRevision
 
 if ($currentTagVersion -eq $newTagVersion) {
