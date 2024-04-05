f2613a5ee61f83f7bf861cdedbacbfa3095bbaa6
Hello <github@devwithkev.com>
2024-04-05 23:00:39 +0200

    Add logic to make git quiet

--- src/subcommands/update.ps1
+++ src/subcommands/update.ps1
@@ -3,15 +3,18 @@ Write-Host "Updating CLI"
 $currentTagRevision = git -C "$PSScriptRoot\..\.." rev-list --tags --max-count=1
 $currentTagVersion = git -C "$PSScriptRoot\..\.." describe --tags $currentTagRevision
 
-git -C "$PSScriptRoot\..\.." fetch --all --tags
+git -C "$PSScriptRoot\..\.." fetch --all --tags --quiet
 
-$newTagRevision = git -C "$PSScriptRoot\..\.." rev-list --tags --max-count=1
-$newTagVersion = git -C "$PSScriptRoot\..\.." describe --tags $newTagRevision
+$newTagRevision = git -C "$PSScriptRoot\..\.." rev-list --tags --max-count=1 --quiet
+$newTagVersion = git -C "$PSScriptRoot\..\.." describe --tags $newTagRevision --quiet
 
 if ($currentTagVersion -eq $newTagVersion) {
-    Write-Host "There is not update"
+    Write-Host "There is no update available"
 }
 else {
-    git -C "$PSScriptRoot\..\.." pull
-    Write-Host "Update is successfulll"
+    $confirmation = Read-Host "A new version $newTagVersion is available, do you want to update now [Y/n]?"
+    if ($confirmation -eq "y") {
+        git -C "$PSScriptRoot\..\.." pull --quiet
+        Write-Host "Update to $newTagVersion version was successfull"
+    }
 }
