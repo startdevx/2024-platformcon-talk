a985baab8b5ab0dcef9871088537ab610812c332
Hello <github@devwithkev.com>
2024-04-06 14:17:34 +0200

    Add sending email capability

---
+++ src/RELEASE_NOTE.md
@@ -0,0 +1,7 @@
+NEW RELEASE NOTE
+
+NEW FEATURE
+Email sending
+
+BUG FIX
+Correct error in subcommand help
--- src/subcommands/update.ps1
+++ src/subcommands/update.ps1
@@ -1,3 +1,25 @@
+function Send-ReleaseNote($releaseVersion) {
+    $secureString = ConvertTo-SecureString $Env:PT_PWD -AsPlainText -Force
+    $credential = New-Object System.Management.Automation.PSCredential ($Env:PT_USER, $secureString)
+
+    $releaseNoteContent = Get-Content "$PSScriptRoot\..\RELEASE_NOTE.md"
+    $releasenote = $releaseNoteContent -join "<br>"
+
+    $sendMailMessageArguments = @{
+        From       = "kevin@startdevx.com"
+        To         = "$Env:USERNAME@devwithkev.com"
+        Subject    = "CLI Release Note $releaseVersion"
+        Body       = $releaseNote
+        BodyAsHtml = $True
+        SmtpServer = "127.0.0.1"
+        Port       = "1025"
+        Credential = $credential
+    }
+
+    Write-Host "Sending release notes to your email address..."
+    Send-MailMessage @sendMailMessageArguments
+}
+
 Write-Host "Checking if new version is available" -ForegroundColor White
 
 git -C "$PSScriptRoot\..\.." fetch --all --tags --quiet
@@ -15,6 +37,9 @@ else {
     $confirmation = Read-Host "A new version $newTagVersion is available, do you want to update now [Y/n]?"
     if ($confirmation -eq "y") {
         git -C "$PSScriptRoot\..\.." pull --quiet
-        Write-Host "Version $newTagVersion was installed successfully!!!"
+        Write-Host "Version $newTagVersion was installed successfully!"
+        Send-ReleaseNote $newTagVersion
     }
 }
+
+exit 0
